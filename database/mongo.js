64c77158bf158839f800ff0d1ad20d6007f788f9
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-03-08 17:39:35 +0000

    create a db services with docker

--- database/mongo.js
+++ database/mongo.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
 const initMongoose = async () => {
+    const DATABASE_URL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27018/chat-app'
+    console.log(DATABASE_URL)
     try {
-        return await mongoose.connect('mongodb://127.0.0.1:27017/chat-app')
+        return await mongoose.connect(DATABASE_URL)
     } catch {
         return Promise.reject('Algo salio terriblemente mal :(')
     }
